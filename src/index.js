e7483bf771d535b175c37fffbfd9847ee3b304cb
mairhead1 <87270879+mairhead1@users.noreply.github.com>
2021-07-22 14:46:02 +0100

    icon and unit conversion

--- src/index.js
+++ src/index.js
@@ -22,11 +22,22 @@ function formatDate(date) {
   
     return `${day} ${hours}:${minutes}`;
   }
+
+  function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu","Fri", "Sat"];
+    
+
+    return days[day];
+  }
   
   function displayWeatherCondition(response) {
+
+    celsiusTemperature = response.data.main.temp
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#temperature").innerHTML = Math.round(
-      response.data.main.temp
+     celsiusTemperature
     );
   
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
@@ -35,6 +46,17 @@ function formatDate(date) {
     );
     document.querySelector("#description").innerHTML =
       response.data.weather[0].main;
+
+    let iconElement = document.querySelector("#icon")
+    iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) ;
+ 
+  }
+
+  function weeklyForecast(){
+    let apiKey = "abe6af7a4e35a49d71bf1d8f7c161176";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={API key}`;
+    axios.get(apiUrl).then(displayForecast);
+
   }
   
   function searchCity(city) {
@@ -49,6 +71,10 @@ function formatDate(date) {
     searchCity(city);
   }
   
+
+
+
+
   function searchLocation(position) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
@@ -72,6 +98,23 @@ function formatDate(date) {
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = 19;
   }
+function displayFahrenheitTemperature(event){
+  event.preventDefault();
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+ 
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+
+let celsiusTemperature = null; 
+
   
   let dateElement = document.querySelector("#date");
   let currentTime = new Date();
@@ -84,4 +127,10 @@ function formatDate(date) {
   currentLocationButton.addEventListener("click", getCurrentLocation);
   
   searchCity("Glasgow");
+
   
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
